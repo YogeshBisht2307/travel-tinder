7c45e892403db1fc6c5fa40f46c9f1a050c3e6c2
Yogesh Bisht <yogesh.bisht@iotfy.co>
2024-06-10 00:31:20 +0530

    deployment related minor changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from 'astro/config';
+import awsAmplify from 'astro-aws-amplify';
 
 export default defineConfig({
-    output: 'server'
+    output: 'server',
+    adapter: awsAmplify()
 });
