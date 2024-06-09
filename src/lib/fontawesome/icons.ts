fdc2ab4fc35522d450ae3463433d27d0bdb47404
Yogesh Bisht <yogesh.bisht@iotfy.co>
2024-06-10 00:12:19 +0530

    home page added

--- src/lib/fontawesome/icons.ts
+++ src/lib/fontawesome/icons.ts
@@ -1,8 +1,9 @@
 import { library, icon } from "@fortawesome/fontawesome-svg-core";
-import {  } from "@fortawesome/free-solid-svg-icons";
+import { faCircleRight } from "@fortawesome/free-solid-svg-icons";
 import { faGoogle } from "@fortawesome/free-brands-svg-icons";
 
 
-library.add(faGoogle);
+library.add(faGoogle, faCircleRight);
 
 export const faGoogleIcon = icon({ prefix: "fab", iconName: faGoogle.iconName });
+export const faCircleRightIcon = icon({prefix: "fas", iconName: faCircleRight.iconName})
